10ada12db9384e63bd04931d5300cc2769cb9c2e
Simon <milkjhang@hotmail.com>
2023-07-15 19:17:18 +0800

    login fn

--- routes/web/auth.js
+++ routes/web/auth.js
@@ -5,11 +5,10 @@ const md5 = require('md5');
 
 // 註冊
 router.get('/reg', (req, res) => {
-  // 響應 HTML 內容
   res.render('auth/reg');
 });
 
-router.post('/reg', (req,res) => {
+router.post('/reg', (req, res) => {
   // password 加密
   const data = {
     ...req.body,
@@ -17,9 +16,32 @@ router.post('/reg', (req,res) => {
   };
   UserModel.create(data)
     .then((result) => {
-      res.render('success', {msg: '註冊成功', url: '/login'})
+      res.render('success', { msg: '註冊成功', url: '/login' })
     })
     .catch((err) => res.status(500).send('這冊失敗'))
 })
+
+// 註冊
+router.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+// 登錄
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const data = {
+    username,
+    password: md5(password)
+  }
+  UserModel.findOne(data)
+    .then((result) => {
+      if (result === null) return res.send('號密碼錯誤');
+
+      res.render('success', { msg: '登錄成功', url: '/login' });
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+});
 
 module.exports = router;
